a8eff17d08d0a9f1c7fcb5d6fc2768ceab8c3e60
Marcin Hubicki <marcin.hubicki@unilodz.eu>
2017-03-25 23:17:54 +0100

    Seems done. Nedds testing

--- Heapsort/Heapsort/ASD2_HubickiM_HeapSort.cpp
+++ Heapsort/Heapsort/ASD2_HubickiM_HeapSort.cpp
@@ -1,3 +1,17 @@
+/*******************************************************************************
+							PROJECT HEAP SORT
+Started on 21.03.2017
+Last Update version 1.3 on : 25.03.2017
+What is new:
+- Many cases are possible now
+- Fixed Heap_sort Function
+
+To do:
+- Need to change printing so it matches the task
+
+Made by Marcin Hubicki
+*******************************************************************************/
+
 #include <iostream>
 #include <algorithm>
 
@@ -19,18 +33,18 @@ void view(int *Tab, int capacity)
 
 void Heap(int * tab, int capacity)
 {
+	capacity -= 1;
 	int iter = 2;
 	int tmp = iter;
 	while (iter <= capacity)
 	{
 		tmp = iter;
+
 		while (tab[tmp]>tab[tmp / 2] && tmp != 1)
 		{
 
 			swap(tab[tmp], tab[tmp / 2]);
-			view(tab, capacity);
 			tmp = tmp / 2;
-
 		}
 		iter++;
 	}
@@ -38,21 +52,38 @@ void Heap(int * tab, int capacity)
 
 void Heap_sort(int * tab, int capacity)
 {
+	capacity -= 1;
+	int cpy_cap = capacity;
+
 	int i = 1, j = 1, tmp, tmp2;
 	int iter = capacity;
 
+
 	while (capacity > 1)
 	{
-
+		j = 1;
 		swap(tab[iter], tab[1]);
 		capacity--;
 
 		tmp = j * 2;
 		tmp2 = (j * 2) + 1;
 
-		while (tmp <= capacity && tmp2 <= capacity)// do poprawy 
+		while (tmp <= capacity || tmp2 <= capacity)
 		{
+
 			tmp = j * 2;
+			if ( tmp >= capacity)
+			{
+				if (tab[j] < tab[capacity])
+				{
+					swap(tab[j], tab[capacity]);
+					break;
+				}
+				else
+				{
+					break;
+				}
+			}
 			tmp2 = (j * 2) + 1;
 
 			if (tab[tmp] > tab[tmp2])
@@ -63,13 +94,13 @@ void Heap_sort(int * tab, int capacity)
 			else
 			{
 				swap(tab[j], tab[tmp2]);
-
 			}
 			j++;
 		}
+		iter--;
 		view(tab, capacity);
 	}
-	//	view(tab, capacity);
+	view(tab, cpy_cap);
 }
 
 
@@ -77,37 +108,32 @@ void Heap_sort(int * tab, int capacity)
 int main()
 {
 
-	int capacity, i = 1;
-	cin >> capacity;
+	int cases;
+	cin >> cases;
 
-	int * Tab = new int[capacity + 1];
-
-	while (i < (capacity + 1))
+	for (int k = 0; k < cases; k++)
 	{
-		cin >> Tab[i];
-		i++;
+
+		int capacity, i = 1;
+		cin >> capacity;
+		capacity += 1;
+
+		int * Tab = new int[capacity];
+
+		while (i < capacity)
+		{
+			cin >> Tab[i];
+			i++;
+		}
+
+		view(Tab, capacity - 1);
+		Heap(Tab, capacity);
+		view(Tab, capacity - 1);
+
+		Heap_sort(Tab, capacity);
+
+		cout << std::endl;
+		delete [] Tab;
 	}
-
-	i = 1;
-	/*while (i < (capacity + 1))
-	{
-		cout << Tab[i] << " ";
-		i++;
-	}
-	cout << std::endl;*/
-	view(Tab, capacity);
-	Heap(Tab, capacity);
-
-	//i = 1;
-	//while (i < (capacity + 1))
-	//{
-	//	cout << Tab[i] << " ";
-	//	i++;
-	//}
-	//cout << std::endl;
-
-	Heap_sort(Tab, capacity);
-
-		system("pause");
 	return 0;
 }
