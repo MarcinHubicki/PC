e7ba85f34fc390d09ea8ce1d3ab54a6ad41b763b
Marcin Hubicki <marcin.hubicki@unilodz.eu>
2017-03-26 23:55:35 +0200

    something still wrong

--- Heapsort/Heapsort/ASD2_HubickiM_HeapSort.cpp
+++ Heapsort/Heapsort/ASD2_HubickiM_HeapSort.cpp
@@ -84,30 +84,48 @@ void Heap_sort(int * tab, int capacity)
 		{
 
 			tmp = j * 2;
-			if ( tmp >= capacity)
+			if ( tmp >= capacity)// tu moze byc blad
 			{
-				if (tab[j] < tab[capacity])
-				{
-					swap(tab[j], tab[capacity]);
-					break;
-				}
-				else
+				if (tmp > capacity)
 				{
 					break;
 				}
+				if (tmp == capacity)
+				{
+					if (tab[capacity] > tab[j])
+					{
+						swap(tab[capacity], tab[j]);
+						break;
+					}
+					else
+					{
+						break;
+					}
+
+				}
+				//if (tab[j] < tab[capacity])
+				//{
+				//	//swap(tab[j], tab[capacity]);
+				//	break;
+				//}
+				//else
+				//{
+				//	break;
+				//}
 			}
 			tmp2 = (j * 2) + 1;
 
 			if (tab[tmp] > tab[tmp2])
 			{
 				swap(tab[j], tab[tmp]);
-
+				j = tmp;
 			}
 			else
 			{
 				swap(tab[j], tab[tmp2]);
+				j = tmp2;
 			}
-			j++;
+			//j++;
 		}
 		iter--;
 		view(tab, capacity);
@@ -154,12 +172,12 @@ int main()
 
 		int * Tab = new int[capacity];
 
-//		Get_Rands(capacity-1, 10000, Tab);
-		while (i < capacity)
-		{
-			cin >> Tab[i];
-			i++;
-		}
+		Get_Rands(capacity-1, 10000, Tab);
+		//while (i < capacity)
+		//{
+		//	cin >> Tab[i];
+		//	i++;
+		//}
 
 		view(Tab, capacity - 1);
 		cout << std::endl;
